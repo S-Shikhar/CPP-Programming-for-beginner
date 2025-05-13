128556954dd8dc1e6f180dc091df049a0f8b5d3b
SOUHARDA SHIKHAR BISWAS <souhardashikharbiswas@gmail.com>
2025-05-13 11:59:38 +0530

    sorting

--- array/bubble.cpp
+++ array/bubble.cpp
@@ -1,21 +1,66 @@
 #include <iostream>
 using namespace std;
+
+class Account
+{
+public:
+    int acno;
+    float balance;
+    
+    Account(int a, float b)
+    {
+        acno = a;
+        balance = b;
+    }
+    void withdraw(int w)
+    {
+        balance -= w;
+    }
+    void deposit(int d)
+    {
+        balance += d;
+    }
+};
+
+class Calculate : public Account
+{
+public:
+    int r, t;
+    float si, amt;
+
+    // Constructor for Calculate class
+    Calculate(int a, float b) : Account(a, b)
+    {
+        r = 0;
+        t = 0;
+        si = 0;
+        amt = 0;
+    }
+
+    void accept(int x, int y)
+    {
+        r = x;
+        t = y;
+        amt = 0;
+    }
+    void compute()
+    {
+        si = (balance * r * t) / 100;
+        amt = balance + si;
+    }
+    void display()
+    {
+        cout << "Account No: " << acno << "\nBalance: " << balance
+             << "\nInterest: " << si << "\nAmount: " << amt << endl;
+    }
+};
 int main()
 {
-    int arr[10];
-    cout << "Enter 10 numbers: ";
-    for (int i = 0; i < 10; i++)
-        cin >> arr[i];
-    for (int i = 0; i < 9; i++)
-        for (int j = 0; j < 9 - i; j++)
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-                
-    cout << "Sorted Array: ";
-    for (int i = 0; i < 10; i++)
-        cout << arr[i] << " ";
+    Calculate c(12345, 10000);
+    c.withdraw(1000);
+    c.deposit(2000);
+    c.accept(5, 2);
+    c.compute();
+    c.display();
     return 0;
 }
